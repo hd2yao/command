2c1539362c77386683c238a35c46e324d0c9558c
hd2yao <1054290355@qq.com>
2023-09-05 15:14:04 +0800

    add Stack method

--- errors/stack.go
+++ errors/stack.go
@@ -19,6 +19,17 @@ func (f StackFrame) String() string {
     return fmt.Sprintf("%s:%d - %s", f.File, f.Line, f.Func)
 }
 
+// Stack 返回错误的堆栈跟踪
+// 错误必须包含堆栈跟踪，或封装一个有堆栈跟踪的错误
+func Stack(err error) []StackFrame {
+    if wErr, ok := err.(wrapperError); ok {
+        return wErr.stack
+    }
+    return nil
+}
+
+// getStack 是 runtime.Callers 的格式化包装器
+// 以 []StackFrame 的形式返回堆栈跟踪
 func getStack(skip int, size int) []StackFrame {
     var (
         pc    = make([]uintptr, size)
